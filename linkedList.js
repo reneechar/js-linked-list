6380e1716e9d0742e78d53b709faeb64ba5a5d47
Renee <rchar20@gmail.com>
2016-09-12 15:49:40 -1000

    15 tests passing, getHead, getTail, and add methods working

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,41 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+	var head = null;
+	var tail = null;
 
-}
+	return {
+		getHead: function() {
+			return head;
+		},
+		
+		getTail: function () {
+			return tail;
+		},
+	
+		add: function(Value) {
+			var newNode = {value: Value, next: null};
+			if (head === null) {
+				head= newNode;
+				tail = newNode;
+			} else {
+				tail.next = newNode;
+				tail = newNode;
+			}
+			return tail;
+		},
+
+		get: function(number) {
+			
+
+		},
+
+		remove: function(number) {
+			return this.value;
+		},
+
+		insert: function(value, number) {
+			return this.value;
+		}
+	};
+};
